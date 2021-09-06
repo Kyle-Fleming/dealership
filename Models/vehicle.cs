0cf7fdd1a1ceae617fcd3e5eef30f1f0ea31b595
kyleathollfleming <kyleathollfleming@gmail.com>
2021-09-06 11:47:21 +0200

    Add project files.

---
+++ Models/vehicle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class vehicle
+    {
+
+        public int ID { get; set; }
+
+        public string name { get; set; } 
+
+        public string category { get; set; }
+
+        public float price { get; set; }
+        
+        public int count { get; set; }
+
+        public string description { get; set; }
+    }
+}
---
+++ Models/vehicleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Models
+{
+    public class vehicleContext : DbContext
+    {
+        public vehicleContext(DbContextOptions<vehicleContext> options)
+            :base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<vehicle> vehicles { get; set; }
+    }
+}
